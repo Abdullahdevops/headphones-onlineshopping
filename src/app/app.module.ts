36febff539356ab713ac028b07fbbfeb9ab77eb3
Abdullah Ghanem <abdallh.fcih@gmail.com>
2018-01-02 18:29:39 +0200

    add components, navbar, header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    HeaderComponent,
+    SliderComponent,
+    FeaturesComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
